368548328f4e167500eb8eb39726abb5db1dfd63
K-Teja <tejachaitu96@gmail.com>
2018-07-02 14:12:13 +0000

    Revert "hal: Port FM support from caf hal"
    This reverts commit 412bfdb86554d7eed2f3aecf429ee76446f3ff36.

--- hal/audio_hw.h
+++ hal/audio_hw.h
@@ -92,7 +92,6 @@ enum {
     USECASE_AUDIO_RECORD_LOW_LATENCY,
     USECASE_AUDIO_RECORD_MMAP,
     USECASE_AUDIO_RECORD_HIFI,
-    USECASE_AUDIO_RECORD_FM_VIRTUAL,
 
     /* Voice extension usecases
      *
@@ -131,9 +130,6 @@ enum {
     /* VOIP usecase*/
     USECASE_AUDIO_PLAYBACK_VOIP,
     USECASE_AUDIO_RECORD_VOIP,
-
-    /* FM usecase */
-    USECASE_AUDIO_PLAYBACK_FM,
 
     AUDIO_USECASE_MAX
 };
